2d48e1dfbed421068c15f978f1a92e7b073ade67
Trevor Aaron Robert Dcosta <63400592+dcostat04@users.noreply.github.com>
2020-08-10 11:09:43 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,66 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+#define max 10
+int top=-1;
+int stk[max];
+void push(char c)
+{
+    if (top ==(max-1))
+    printf("stack overflow\n");
+    else
+    {
+        top=top+1;
+        stk[top]=c;
+    }
+}
+char pop()
+{
+    if (top==-1)
+    printf("stack underflow\n");
+    else
+    return(stk[top--]);
+}
+void main()
+{
+    char expe[max],temp;
+    int i,flag=1;
+    printf("enter an expression\n");
+    gets(expe);
+    for (i=0;i<strlen(expe);i++)
+    {
+        if (expe[i]=='('||expe[i]=='{'||expe[i]=='[')
+         push(expe[i]);
+        if(expe[i]==')'|| expe[i]=='}'||expe[i]==']')
+         if(top==-1)
+             flag=0;
+        else
+        {
+            temp=pop();
+            if(expe[i]==')'&&(temp=='{'||temp=='['))
+              flag=0;
+             if(expe[i]=='}'&&(temp=='('||temp=='['))
+              flag=0;
+            if(expe[i]==']'&&(temp=='('||temp=='{'))
+             flag=0;
+              
+        }
+    }
+    if(top>=0)
+      flag=0;
+    if (flag==1)
+    printf("valid expression\n");
+    else
+    printf("invalid expression\n");
+    
+    
+}
+
